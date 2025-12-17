8284f395ac7915b16d5f5a508666ce7e3694568e
ismail parlak <ihpar@ismails-MacBook-Air.local>
2025-12-17 09:38:10 +0300

    init

---
+++ classes.py
@@ -0,0 +1,20 @@
+classes = ["Air",
+           "E 100", "E 97.5", "E 95", "E 90", "E 80", "E 50"]
+colors = ["#2c73d2",
+          "#cdd0ff", "#afdfff", "#91edff", "#88f8ef", "#a0fecd", "#cefeac"]
+AIR = 0
+ETH_100 = 1
+ETH_97_5 = 2
+ETH_95 = 3
+ETH_90 = 4
+ETH_80 = 5
+ETH_50 = 6
+
+regression_targets = {
+    ETH_100: 100,
+    ETH_97_5: 95,
+    ETH_95: 90,
+    ETH_90: 80,
+    ETH_80: 70,
+    ETH_50: 50
+}
