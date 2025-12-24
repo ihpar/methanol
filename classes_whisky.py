f64f76ac6d4baa509dd9ffbefbb5113a42f6b3ee
ismail h parlak <ismail.hakki.parlak@gmail.com>
2025-12-24 14:49:36 +0300

    adds labels

---
+++ classes_whisky.py
@@ -0,0 +1,18 @@
+classes = ["Air",
+           "M 0", "M 3", "M 7", "M 10", "M 15"]
+colors = ["#2c73d2",
+          "#cdd0ff", "#afdfff", "#91edff", "#88f8ef", "#a0fecd"]
+AIR = 0
+METH_0 = 1
+METH_3 = 2
+METH_7 = 3
+METH_10 = 4
+METH_15 = 5
+
+regression_targets = {
+    METH_0: 0,
+    METH_3: 3,
+    METH_7: 7,
+    METH_10: 10,
+    METH_15: 15
+}
